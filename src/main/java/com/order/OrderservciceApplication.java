6889934acea12d9b99db0f96084a2865492b5b43
tarakeshpolakolu <tarakeshpolakolu@gmail.com>
2024-06-29 21:12:34 +0530

    Initial Commit

---
+++ src/main/java/com/order/OrderservciceApplication.java
@@ -0,0 +1,13 @@
+package com.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderservciceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderservciceApplication.class, args);
+	}
+
+}
